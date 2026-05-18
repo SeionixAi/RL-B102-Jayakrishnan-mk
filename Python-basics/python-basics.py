861c997b6514ee00c2f7f1d7f256f83f652ecc10
Jayakrishnan-mk <jayakrishnansfc43@gmail.com>
2026-05-18 22:40:12 +0530

    python basic programs

---
+++ Python-basics/python-basics.py
@@ -0,0 +1,35 @@
+# basic prgrm--------
+# name = input("Enter your name: ");
+# age = input("Enter your age: ");
+
+# print(f"My name is {name} and im {age} years old.hahaha")
+
+# even or odd--------
+# num = int(input("Enter a number: "));
+# if num % 2 == 0:
+#     print("even");
+# else :
+#     print("odd");
+
+# list ops--------
+# numbers = [10,20,30,40,50];
+
+# print("Min: ", min(numbers))
+# print("Max: ", max(numbers))
+# print("Sum: ", sum(numbers))
+    
+# function example--------
+# def square(a):
+#     return a*a;
+    
+# print(square(3));
+
+# loop--------
+# for i in range(0,5):
+#     print(i)
+# i=0;
+
+# while(i<5):
+#     print(i)
+#     i+=1;
+
