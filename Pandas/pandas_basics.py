3a740dd4ee556571a1a13170b069ddd919e237a3
Jayakrishnan-mk <jayakrishnansfc43@gmail.com>
2026-05-23 00:13:39 +0530

    fix(pandas): section updated

--- Pandas/pandas_basics.py
+++ Pandas/pandas_basics.py
@@ -1,15 +1,5 @@
 import pandas as pd;
 
-# df = pd.DataFrame(data);
-# print(df);
-
-# read csv
-df = pd.read_csv('data.csv');
-print(df.head());
-
-# print("Available columns:", df.columns.tolist())
-# print(df["Age"].mean())
-
-# print(df["Marks"].max())
-
-# print(df[df["Age"] > 22]);
+# Read CSV
+df = pd.read_csv("data.csv")
+print(df.head())
