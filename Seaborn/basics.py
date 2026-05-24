d0afafd3e2af1b9107dbdbaa0aaf3d990708bf05
Jayakrishnan-mk <jayakrishnansfc43@gmail.com>
2026-05-24 17:27:30 +0530

    feat(seaborn): new library started

---
+++ Seaborn/basics.py
@@ -0,0 +1,12 @@
+
+# import seaborn as sns
+# import pandas as pd
+
+# df = pd.DataFrame({
+#     "x":[1,2,3],
+#     "y":[10,20,30]
+# })
+
+# sns.lineplot(data=df, x="x", y="y")
+
+# sns.heatmap(df.corr(), annot=True)
